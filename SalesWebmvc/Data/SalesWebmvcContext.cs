853f4e0aa7291c7ff459eb6b14c8fa33ff1a4ee1
Tiago Pereira Barreto <93627992+TiagoPBarreto@users.noreply.github.com>
2023-02-14 21:00:59 -0300

    Project Initial

---
+++ SalesWebmvc/Data/SalesWebmvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebmvc.Models
+{
+    public class SalesWebmvcContext : DbContext
+    {
+        public SalesWebmvcContext (DbContextOptions<SalesWebmvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebmvc.Models.Departament> Departament { get; set; }
+    }
+}
---
+++ SalesWebmvc/Models/viewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebmvc.Models.viewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
